341ae22a598323406449eb19f538e737f39af9de
Johan <johan.ekberg.666@gmail.com>
2025-06-07 15:27:46 +0200

    Update to Compose BOM 2025.06.00.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 }
 
 dependencies {
-
+    implementation(platform(libs.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
